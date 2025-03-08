6a6f5502c0595f27720d8b6bf0c6a22088bf5872
難波隼人 <hayatonanba@gmail.com>
2025-03-09 06:19:24 +0900

    feat: route-api

---
+++ src/server/routes/blogRoutes.ts
@@ -0,0 +1,70 @@
+import { createRoute, z } from "@hono/zod-openapi";
+import { BlogIdSchema, BlogSchema, BlogsSchema, CreateBlogSchema } from "../models/blogSchemas";
+
+export const getBlogsRoute = createRoute({
+  path: "/",
+  method: "get",
+  description: "ブログの全データを取得",
+  responses: {
+    200: {
+      description: "取得成功",
+      content: {
+        "application/json": {
+          schema: BlogsSchema
+        }
+      }
+    }
+  }
+})
+
+export const getBlogRoute = createRoute({
+  path: "/{id}",
+  method: "get",
+  description: "個別記事の取得",
+  request: {
+    params: BlogIdSchema
+  },
+  responses: {
+    200: {
+      description: "取得成功",
+      content: {
+        "application/json": {
+          schema: BlogSchema
+        }
+      }
+    },
+    404: {
+      description: "記事みつかんなかった",
+      content: {
+        "application/json": {
+          schema: z.null()
+        }
+      }
+    }
+  }
+})
+
+export const createBlogRoute = createRoute({
+  path: "",
+  method:"post",
+  description: "投稿",
+  request: {
+    body: {
+      content: {
+        "application/json": {
+          schema: CreateBlogSchema
+        }
+      }
+    }
+  },
+  responses: {
+    201: {
+      description: "成功",
+      content: {
+        "application/json": {
+          schema: BlogSchema
+        }
+      }
+    }
+  }
+})
